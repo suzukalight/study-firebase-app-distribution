b076594f2bb244669c22986eeab75ef2ca202e08
suzukalight <mail@mkubara.com>
2023-12-18 18:51:09 +0900

    fix: android package name

--- android/app/src/main/java/com/suzukalight/studyfirebaseappdistribution/MainActivity.kt
+++ android/app/src/main/java/com/suzukalight/studyfirebaseappdistribution/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.studyfirebaseappdistribution
+package com.suzukalight.studyfirebaseappdistribution
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
--- android/app/src/main/java/com/suzukalight/studyfirebaseappdistribution/MainApplication.kt
+++ android/app/src/main/java/com/suzukalight/studyfirebaseappdistribution/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.studyfirebaseappdistribution
+package com.suzukalight.studyfirebaseappdistribution
 
 import android.app.Application
 import com.facebook.react.PackageList
